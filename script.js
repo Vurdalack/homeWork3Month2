66054162b1c77314454cadcdebd04bd375478d45
Vurdalack <chyngyz887@gmail.com>
2024-02-18 18:01:47 +0600

    initial

---
+++ script.js
@@ -0,0 +1,348 @@
+const obj = {
+    name: 'Chyngyz',
+    last_name: 'Zhumanazarov',
+    phone: '+996 222 66 77 30',
+    meried: false,
+    gender: 'male',
+    address: {
+        street: 'Dyikan-Kyshtack-12',
+        number: 12,
+        country: 'KG',
+        region: 'osh',
+        city: 'osh'
+    }, school: {
+        name: 'Kirov',
+        address: {
+            street: 'abdimatalip amatova',
+            number: 4,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'
+        },
+        frends: [
+            {
+                name: 'arnal',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: false,
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                }, school: {
+                    name: 'Kirov',
+                    address: {
+                        street: 'abdimatalip amatova',
+                        number: 4,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    },
+                }, famaly: {
+                    name: 'father',
+                    role: 'father',
+                    last_name: 'usmanov',
+                    phone: '+996 732 21 84 66',
+                    meried: true,
+                    gender: 'male',
+                    address: {
+                        street: 'petrova',
+                        number: 12,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    },
+                }, name: 'mother',
+                role: 'mother',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: true,
+                gender: 'women',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+                name: 'brother',
+                role: 'brother',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                }, name: 'sister',
+                role: 'sister',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                gender: 'women',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                }
+            },
+            {
+                name: 'Sasha',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: false,
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                }, school: {
+                    name: 'Kirov',
+                    address: {
+                        street: 'abdimatalip amatova',
+                        number: 4,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    }, famaly: {
+                        name: 'Batya',
+                        role: 'father',
+                        last_name: 'usmanov',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'male',
+                        address: {
+                            street: 'petrova',
+                            number: 12,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        }, name: 'maman',
+                        role: 'mother',
+                        last_name: 'usmanov',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'women',
+                        address: {
+                            street: 'petrova',
+                            number: 12,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        }, name: 'kira',
+                        role: 'sister',
+                        last_name: 'usmanov',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'women',
+                        address: {
+                            street: 'petrova',
+                            number: 12,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        }, name: 'danchik',
+                        role: 'big bro',
+                        last_name: 'usmanov',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'male',
+                        address: {
+                            street: 'petrova',
+                            number: 12,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        }, name: 'kiril',
+                        role: 'midle bro',
+                        last_name: 'usmanov',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'male',
+                        address: {
+                            street: 'petrova',
+                            number: 12,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        }, name: 'kolyan',
+                        role: 'mini bro',
+                        last_name: 'usmanov',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'male',
+                        address: {
+                            street: 'petrova',
+                            number: 12,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        },
+                    },
+                },
+
+
+
+
+                name: 'Arsen',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: true,
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                }, workplace: {
+                    name: 'geeks',
+                    address: {
+                        street: 'abdimatalip amatova',
+                        number: 2,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    }, school: {
+                        name: 'Kirov',
+                        address: {
+                            street: 'abdimatalip amatova',
+                            number: 4,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh',
+                        },
+                    },
+                },
+                famaly: {
+                    name: 'otec',
+                    role: 'father',
+                    last_name: 'usmanov',
+                    phone: '+996 732 21 84 66',
+                    meried: true,
+                    gender: 'male',
+                    address: {
+                        street: 'petrova',
+                        number: 12,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    }
+                },
+                famaly: {
+                    name: 'Venera',
+                    role: 'moter',
+                    last_name: 'usmanov',
+                    phone: '+996 732 21 84 66',
+                    meried: true,
+                    gender: 'women',
+                    address: {
+                        street: 'petrova',
+                        number: 12,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    },
+                    workplace: {
+                        name: 'geeks',
+                        address: {
+                            street: 'abdimatalip amatova',
+                            number: 2,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        },
+                    },
+                }
+            },
+        ],
+        
+        },
+        famaly: [
+            {
+                skills: [
+                    'ts', 'js', 'go', 'py'
+                ],
+
+                name: 'Zhyrgal',
+                role: 'father',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: true,
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+            {
+                name: 'Maya',
+                role: 'mother',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: true,
+                gender: 'women',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+            {
+                name: 'Dastan',
+                role: 'brother',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: true,
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            }, {
+                name: 'Aisha',
+                role: 'sister',
+                last_name: 'usmanov',
+                phone: '+996 732 21 84 66',
+                meried: true,
+                gender: 'male',
+                address: {
+                    street: 'petrova',
+                    number: 12,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+
+        ]
+        ,
+        skills: [
+            'ts', 'js', 'go', 'py',
+        ],
+    
+        
+
+    }
+   
+
+
+
+console.log(obj.famaly[1]);
+
